4c03504b25d915463af5622b6a7d73123ffd3630
shreeteja22 <smutukundu2206@gmail.com>
2025-07-17 17:39:10 +0530

    fix: pnpm build errors

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/signup" element={<Signup />} />
+          {/* <Route path="/" element={<Navigate to="/dashboard" replace />} /> */}
+          <Route path="/" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send" element={<SendMoney />} />
